e93797fc3d74371e733e2f2ca26a1ef2c864512d
Yassine Rafa <yassine.rafa@outlook.com>
2016-04-14 18:48:49 +0100

    Add a form in the sign up page and sign up errors messages

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -42,12 +42,19 @@ class UserTest < ActiveSupport::TestCase
 
 	test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-                           foo@bar_baz.com foo@bar+baz.com]
+                           foo@bar_baz.com foo@bar+baz.com foo@bar..com]
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end
+
+	test "email adresses should be saved as lower-case" do
+		mixed_case_email = "Foo@ExAmplE.com"
+		@user.email = mixed_case_email
+		@user.save
+		assert_equal mixed_case_email.downcase, @user.reload.email
+	end
 
   test "email addresses should be unique" do
     duplicate_user = @user.dup 
