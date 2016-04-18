5447c099fb97799ef168d0859a1f23ea1f64af10
Yassine Rafa <yassine.rafa@outlook.com>
2016-04-18 08:25:26 +0100

    Finish log in/log out

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,6 +8,26 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical
   # order.
   fixtures :all
-
+  # Returns true if a test user is logged in.
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+  # Log in a test user
+  def log_in_as(user, options = {})
+  	password = options[:password] || 'password'
+  	remember_me = options[:remember_me] || '1'
+  	if integration_test?
+  		post login_path, session: {email: user.email,
+  															 password: password,
+  															 remember_me: remember_me}
+  	else
+  		seesion[:user_id] = user.user_id
+  	end
+  end
+  
+  # Returns true inside an integration test.
+    def integration_test?
+      defined?(post_via_redirect)
+    end
   # Add more helper methods to be used by all tests here...
 end
